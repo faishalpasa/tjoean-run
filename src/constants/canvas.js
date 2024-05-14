7f35d66b899e31d62183338bd7666a79f93a4959
Faishal Pasa <faishal.pasa@bareksa.com>
2024-05-14 15:22:41 +0700

    fix: mobile detect

--- src/constants/canvas.js
+++ src/constants/canvas.js
@@ -1,5 +1,7 @@
-export const WIDTH = window.innerWidth
-export const HEIGHT = window.innerHeight
+const isMobile = !!navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)
+
+export const WIDTH = isMobile ? window.innerWidth : 800
+export const HEIGHT = isMobile ? window.innerHeight : 400
 
 export const TILE_MULTIPLIER = 2
 export const TILE_HEIGHT = 32
